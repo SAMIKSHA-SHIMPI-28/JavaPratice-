93f7c1643fc15b468302cec8ca26297ecd3de8e5
Samiksha Shimpi <samikshashimpi904@gmail.com>
2025-07-23 12:19:15 +0530

    variable maim

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        // Printing Hello messages
+        System.out.println("Hello and Welcome To First Java Code!");
+
+
+
+        boolean A1 = true;
+        boolean A2 = false;
+        char grade = 'A';
+        int age = 21;
+        float height = 4.2f;
+        double pi = 3.1415926535;
+        long population = 1400000000L;
+        String message = "Learning Java with data types!";
+        byte myByte = 25;
+        short myShort = 32000;
+
+        // Printing all variable values
+
+        System.out.println("Is Java Fun: " + A1);
+        System.out.println("Is fish tasty? " + A2);
+        System.out.println("Char value: " + grade);
+        System.out.println("Int value: " + age);
+        System.out.println("Float value: " + height);
+        System.out.println("Double value: " + pi);
+        System.out.println("Long value: " + population);
+        System.out.println("String value: " + message);
+        System.out.println("byte: " + myByte);
+        System.out.println("short: " + myShort);
+    }
+}
